4148d6b225444d43ed96d863c1c7880fdbde4c7b
Josh Eisenthal <joshuae@netapp.com>
2020-01-22 15:53:14 -0500

    installed phpunit

--- php/mongo.php
+++ php/mongo.php
@@ -3,23 +3,40 @@
 	require 'vendor/autoload.php'; // include Composer's autoloader
 
 	$client = new MongoDB\Client("mongodb://localhost:27017");
-	$collection = $client->demo->beers;
+	// $collection = $client->demo->beers;
 
 	// Collections in Zwapp are initialized via curated scrapes from the ComicVine wiki. 
 	// Accessing Zwapp DB objects transparently get and cache properties needed from the ComicVine API.
 	
 	abstract class Document {
-		private $collection;
-		private $id;
-		private $cv_init;
+		// private $collection;
+		// private $id;
 
-		function __construct($id, $collection) {
-			$this->id = $id;
+		function __construct(ComicVine\Query $cv_query, MongoDB\Collection $collection) {
+			$this->cv_query = $cv_query;
 			$this->collection = $collection;
 		}
 
 		function __get($property) {
-			
+			// id accessible via cv_query:
+			if ($property == "id") {
+				return $this->cv_query->id;
+			}
+			$doc = $this->collection.find({['_id' => $this->id]})->toArray()[0];
+
+			if (!$doc->cv_init) {
+				$this->setData();
+			}
+		}
+
+		function getSetDocument() {
+			$doc = $this->collection.find({['_id' => $this->id]})->toArray()[0];
+
+			if (!$doc->cv_init) {
+				$this->setData();
+			}
+
+
 		}
 	}
 
